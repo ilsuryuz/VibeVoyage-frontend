185d3cd1469fb68841c20772a791403ab9a33946
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2022-10-04 15:45:39 -0400

    update NotesPage and notes components

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -4,9 +4,9 @@ const Notes = (props) => {
   return (
     <div className="Notes">
       <h1>Notes</h1>
-      <h5 className="notes-name">{props.name}</h5>
+      {/* <h5 className="notes-name">{props.name}</h5>
       <h5 className="notes-title">{props.title}</h5>
-      <p className="notes-content">{props.content}</p>
+      <p className="notes-content">{props.content}</p> */}
     </div>
   )
 }
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,31 +1,103 @@
 import {useState} from "react"
 import Notes from "../components/Notes"
 import Main from "../components/Main"
+import { Link } from "react-router-dom"
 
-function NotesPage({notes}, {createNotes}) {
-  
-const notemap = notes.map((ele, index) => {
-    return(
-      <div>
-        <h1>hi</h1>
-      <Notes
-       name={ele.name}
-        title={ele.title}
-        content={ele.content}
-        key={index}        
-        />
+function NotesPage(props) {
+
+  // loaded function
+  const loaded = () => {
+    return props.notes.map((note) => (
+      <div key={note._id} className="note">
+        <Link to={`/notes/${notes._id}`}><h1>{notes.name}</h1>
+        </Link>
+        <h3>{notes.title}</h3>
       </div>
-    )
-  }
+    ));
+  };
 
-return (
- <div className="Notes">
-   <h1>Notes</h1>
-         <section>
-           {notemap}
-         </section>
-     </div>
-   )  
-npm }
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  return props.notes ? loaded() : loading();
+}
 
 export default NotesPage;
+
+import { useState } from "react";
+import { Link } from "react-router-dom"
+import Main from "../components/Main"
+import NotesPage from "../pages/NotesPage";
+
+function Notes(props) {
+  // state to hold formData
+  const [ newForm, setNewForm ] = useState({
+    name: "",
+    title: "",
+    content: "",
+  });
+
+  // handleChange function for form
+  const handleChange = (event) => {
+    setNewForm({ ...newForm, [event.target.name]: event.target.value });
+  };
+
+  // handle submit function for form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.createNotes(newForm);
+    setNewForm({
+      name: "",
+      title: "",
+      content: "",
+    });
+  };
+
+  // loaded function
+  const loaded = () => {
+    return props.notes.map((note) => (
+      <div key={note._id} className="note">
+        <Link to={`/notes/${note._id}`}><h1>{note.name}</h1></Link>
+        <h3>{note.title}</h3>
+        <h3>{note.content}</h3>
+      </div>
+    ));
+  };
+
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newForm.name}
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.title}
+          name="title"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.content}
+          name="content"
+          placeholder="content URL"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Create Note" />
+      </form>
+      {props.notes ? loaded() : loading()}
+    </section>
+  );
+}
+
+export default Notes;
