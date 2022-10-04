328aa9029fd14844d80f093541c6c5858005408c
Oma Withers <omacwitherssei@gmail.com>
2022-10-04 15:53:55 -0400

    "

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom"
 
-const Videos = () => {
+function VideoPage (props) {
+
+  const [ newForm, setNewForm ] = useState({
+    name: "",
+    url: "",
+  });
+
+
+  const handleChange = (event) => {
+    setNewForm({ ...newForm, [event.target.name]: event.target.value });
+  };
+
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.createPeople(newForm);
+    setNewForm({
+      name: "",
+      image: "",
+      title: "",
+    });
+  };
+
+ 
+  const loaded = () => {
+    return props.videos.map((person) => (
+      <div key={person._id} className="person">
+        <Link to={`/videos/${video._id}`}><h1>{video.name}</h1></Link>
+        <img src={person.image} alt={person.name} />
+        <h3>{video.title}</h3>
+      </div>
+    ));
+  };
+
+  const loading = () => {
+    return <h1>Vibe Loading...</h1>;
+  };
+
   return (
-    <div>Videos</div>
-  )
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newForm.name}
+          name="video name"
+          placeholder="video name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.image}
+          name="video"
+          placeholder="video URL"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.title}
+          name="title"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Add Video" />
+      </form>
+      {props.videos ? loaded() : loading()}
+    </section>
+  );
 }
 
-export default Videos
+export default VideoPage;
