19d59a7fb017661470a72d6bc1cd85d3d71a76ee
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2022-10-04 11:41:34 -0400

    update notes page

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import {useState} from "react"
+import Notes from "../components/Notes"
+import Main from "../components/Main"
 
-const Notes = () => {
-  return (
-    <div>Notes</div>
-  )
-}
+function NotesPage({notes}, {createNotes}) {
+  
+  // const notemap = notes.map((ele, index) => {
+    return(
+      <div>
+        <h1>hi</h1>
+      <Notes
+        // name={ele.name}
+        // title={ele.title}
+        // content={ele.content}
+        // key={index}        
+        />
+      </div>
+    )
+  }
 
-export default Notes
+//   return (
+//     <div className="Notes">
+//       <h1>Notes</h1>
+//         <section>
+//           {notemap}
+//         </section>
+//     </div>
+//   )  
+// }
 
+export default NotesPage;
