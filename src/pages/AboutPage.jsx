0dbaf9fce078147e11d7e58b04814056dabfbc0a
Oma Withers <omacwitherssei@gmail.com>
2022-10-04 11:44:52 -0400

    "Added Text Content To About Page"

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className= "AboutText">
+      <h1>About Us</h1>
+      <h3>At VibeVoyage we take pride in serving our users 
+        by giving them a space that keeps their mental clarity in mind. 
+        We are composed of a mindful group that believes in relaxation, 
+        organization, efficiency and intention. </h3>
+<h2>Commemorating You</h2>
+<h3>In life things can get hectic. Even forgetting simple things such as remembering to breathe or drinking water can easily slip by us.
+   Our application will help you remember even the little things to keep you focused on YOU. </h3>
+   <h2>Do More But, With Spice</h2>
+   <h3>With stress things can become very unclear. Doing a simple task can take ages in the presence of trying circumstances. 
+    With our application you will be able to wind down and vibe out with just a few taps on your screen. </h3>
+   <h2>Growing YOU</h2>
+   <h3>Ultimately at the core becoming whole is the ability to grow. While we may not be able to change the world at the time, 
+    we take great pride in helping you complete the actions that will lead to better habits and overall a higher quality of life. 
+</h3>
+   <h1>Our Team</h1>
+      
+      </div>
   )
 }
 
