7f600673f7ca0e24a13547517f06aa99f3594b33
Ilsur <yuzlikeyevi@gmail.com>
2022-10-06 02:14:04 -0500

    Completed: Able to login and logout with Google Auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { auth } from "./services/firebase";
 import Main from "./components/Main";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [ user, setUser ] = useState(null);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {setUser(user)});
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="App">
-    < Header />
-    <Main />
+    <Header user={user}/>
+    <Main user={user}/>
     <Footer />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
+import { login, logout } from '../services/firebase'
 import { Link } from 'react-router-dom'
 
-function Header (props) {
-  return(
-  <header className="header">
-    <Link to='/'>
-      <h1>Vibe Voyage</h1>
-    </Link>
-    <nav>
-      <ul className="navLinks">
-        <li><Link to="/about">About</Link></li>
-        <li><Link to='/login'>Login</Link></li>
-        <li><Link to='/register'>Register</Link></li>
-      </ul>
-    </nav>
-  </header>
+function Header(props) {
+  return (
+    <header className="header">
+      <Link to='/'>
+        <h1>Vibe Voyage</h1>
+      </Link>
+      <nav>
+        <ul className="navLinks">
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        {
+          props.user ?
+            <>
+              <div>VibeChecked, {props.user.displayName}</div>
+              <img src={props.user.photoURL} alt={props.user.displayName} style={{
+                height: 35,
+                width: 'auto',
+                borderRadius: '50%',
+                marginRight: 10
+              }}></img>
+              <div onClick={logout}
+                style={{
+                  cursor: 'pointer',
+                  marginRight: 10
+                }}>Logout</div>
+            </>
+            :
+            <div onClick={login}
+              style={{
+                cursor: 'pointer',
+                marginRight: 10
+              }}>Login</div>
+        }
+      </nav>
+    </header>
 
 
   )
