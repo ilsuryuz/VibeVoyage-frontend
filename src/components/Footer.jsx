8cd340922659744078744b284bdaf6bd2484dbaa
Brandon Hernandez <bhernandez30497@gmail.com>
2022-10-06 13:47:47 -0400

    initial styling for footer and header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,14 @@ import { Link } from "react-router-dom"
 
  function Footer (props) {
     return (
-        <footer className = 'footer'>
-            <nav>
+        <footer>
             <ul className = 'footerLinks'>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to = "/notes">Notes</Link></li>    
-                <li><Link to = "/meditation">Meditation</Link></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to = "/notes">Notes</Link></li> 
+            <li><Link to = "/meditation">Meditation</Link></li>
             </ul>
-            </nav>
-        </footer>
+        </footer>    
+
     )
 }
 
